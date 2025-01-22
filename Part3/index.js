42fcb304cc866112a5bb7995a25b0a4675c707e3
Hesam V. <hesam.vahib@gmail.com>
2025-01-22 20:07:21 +0200

    using morgan customFormat

--- Part3/index.js
+++ Part3/index.js
@@ -2,18 +2,12 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-app.use(morgan('tiny'))
 app.use(express.json())
 
-// const requestLogger = (request, response, next) => {
-//   console.log('Method:', request.method)
-//   console.log('Path:  ', request.path)
-//   console.log('Body:  ', request.body)
-//   console.log('---')
-//   next()
-// }
+morgan.token('body', (req) => JSON.stringify(req.body));
+const customFormat = ':method :url :status :res[content-length] - :response-time ms :body';
 
-// app.use(requestLogger)
+app.use(morgan(customFormat))
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
